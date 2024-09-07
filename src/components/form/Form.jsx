233a602ade8b11e432bdefb4c0c3f9716e8b09f2
Alexander-Agu <theonlyagu@gmail.com>
2024-09-07 20:31:33 +0200

    Finished styling the form component

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import './form.css'
 
 function Form() {
-    const [form, setForm] = useState({firstName:'', lastName: ''})
+    const [form, setForm] = useState({firstName:'', lastName: '', task: ''})
   return (
     <section>
-        <h1 className='logoName'>Dev-Notes</h1>
+        <div className="logo">
+            <h1 className='logoName'>Dev-Notes</h1>
+        </div>
 
         <div className="formContainer">
             <div className="form">
@@ -18,6 +20,23 @@ function Form() {
                     <div className="lastName">
                         <h2>Lastname</h2>
                         <input onChange={(e) => setForm({...form, lastName: e.target.value})} type="text" value={form.lastName} />
+                    </div>
+                </div>
+                
+                <div className="taskContainer">
+                    <h2>Task?</h2>
+                    <input className='taskInput' onChange={(e) => setForm({...form, task: e.target.value})} type="text" value={form.task}/>
+                </div>
+
+                <div className="datesContainer">
+                    <div className="todayDate">
+                        <h2>Today's Date:</h2>
+                        <input type="text" />
+                    </div>
+
+                    <div className="submitDate">
+                        <h2>Submit Date:</h2>
+                        <input type="text" placeholder='DD\MM\YYYY'/>
                     </div>
                 </div>
             </div>
